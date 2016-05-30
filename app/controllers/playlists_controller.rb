475ed8a02565860cdf67599376131b3aa601cbaf
Robbie Jaeger <jaeger.rob@gmail.com>
2016-05-30 17:07:57 -0600

    Refactor views to add partials and before_action methods in controllers

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,11 +1,11 @@
 class PlaylistsController < ApplicationController
+  before_action :set_playlist, only: [:show, :edit, :update]
 
   def index
     @playlists = Playlist.all
   end
 
   def show
-    @playlist = Playlist.find(params[:id])
   end
 
   def new
@@ -18,17 +18,18 @@ class PlaylistsController < ApplicationController
   end
 
   def edit
-    @playlist = Playlist.find(params[:id])
   end
 
   def update
-    @playlist = Playlist.find(params[:id])
     @playlist.update(playlist_params)
-
     redirect_to @playlist
   end
 
   private
+
+  def set_playlist
+    @playlist = Playlist.find(params[:id])
+  end
 
   def playlist_params
     params.require(:playlist).permit(:name, song_ids: [])
